bdd3154cc12fb76d90e749b46e7634d149e803a6
swalter67 <swalter67@gmail.com>
2024-04-21 15:26:28 +0200

    enfin

--- .vagrant/bundler/global.sol
+++ .vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["net-ssh",[">= 2.6.5"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]]],"checksum":"423c5fa4858009ecc5379c80b791ffc8e882a47fce6754302801e06078159626","vagrant_version":"2.2.9"}
+{"dependencies":[["net-ssh",[">= 2.6.5","< 8.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]]],"checksum":"ff2c678fc1c2ec4fb3113e9fb1fece83b2936f00524c4fe545d0b89fd741e1ac","vagrant_version":"2.4.1"}
