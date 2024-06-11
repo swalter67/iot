252d55250468c92a136deebcae318be7853337b5
loundor <loundor@gmail.com>
2024-06-11 17:36:58 +0200

    Last problem on p2

--- stef/p2/script/install.sh
+++ stef/p2/script/install.sh
@@ -1,5 +1,20 @@
-!/bin/bash
+#!/bin/sh
 
+# install Helm 
+curl -o /tmp/helm.tar.gz -LO https://get.helm.sh/helm-v3.10.1-linux-amd64.tar.gz
+tar -C /tmp/ -zxvf /tmp/helm.tar.gz
+mv /tmp/linux-amd64/helm /usr/local/bin/helm
+chmod +x /usr/local/bin/helm
 
-kubectl apply -f /vagrant/confs/traefik-deployement.yaml >/dev/null 2>&1
-kubectl apply -f /vagrant/confs/depl.yaml >/dev/null 2>&1
+APP_VERSION="1.10.1"
+CHART_VERSION="4.10.1"
+sleep 1
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+sleep 1
+helm search repo ingress-nginx --versions
+helm template ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --version ${CHART_VERSION} --namespace ingress-nginx > /vagrant/confs/nginx-ingress.${APP_VERSION}.yaml
+
+kubectl create namespace ingress-nginx
+kubectl apply -f /vagrant/confs/nginx-ingress.${APP_VERSION}.yaml
+kubectl apply -f /vagrant/confs/depl.yaml
+
