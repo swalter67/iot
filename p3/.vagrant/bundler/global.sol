2fd6941a311a5443e996779b8757f9a63c2e39ed
loundor <loundor@gmail.com>
2024-06-18 18:02:57 +0200

    test p3

--- p3/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["racc",["~> 1.4"]],["nokogiri",["~> 1.6"]],["iniparse",["> 1.0"]],["log4r",["~> 1.1"]],["concurrent-ruby",["~> 1.0"]],["i18n",["~> 1.8"]],["vagrant-vmware-esxi",["= 2.5.5"]]],"checksum":"15da62c3ba135223a252ecb97b7e21966c027c7fdb4a9072e28a546c5d308684","vagrant_version":"2.4.1"}
